ab2ebd1c2ad9f944274806dc534bd1302de8f214
syp386 <syp386@PC-syp386.neuxa.com>
2019-12-23 11:03:05 +0800

    Merge origin/master
    Conflicts:
    	GoShreddingPrototype/src/goshredding/data/EventCellRender.java
    	GoShreddingPrototype/src/goshredding/data/RecommandedEventCellRender.java
    	GoShreddingPrototype/src/goshreddingPrototype/MainFormUI.java

--- src/com/tony/goshredding/ui/OpenEventsUI.java
+++ src/com/tony/goshredding/ui/OpenEventsUI.java
@@ -452,13 +452,13 @@ public class OpenEventsUI extends javax.swing.JDialog {
     }// </editor-fold>//GEN-END:initComponents
 
     private void manageMembersBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_manageMembersBtnActionPerformed
-        MembersManagementUI mmFrm = new MembersManagementUI(null, true);
+        membersManagementUI mmFrm = new membersManagementUI(null, true);
         mmFrm.setVisible(true);
     }//GEN-LAST:event_manageMembersBtnActionPerformed
 
     private void reviewDeleteBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_reviewDeleteBtnActionPerformed
         if (GoService.currentUserType == GoService.USER_TYPE_PARTICIPANT) {
-            WriteReviewUI wrFrm = new WriteReviewUI(null, true);
+            writeReviewUI wrFrm = new writeReviewUI(null, true);
             wrFrm.eventId = event.eventId;
             wrFrm.setVisible(true);
 
